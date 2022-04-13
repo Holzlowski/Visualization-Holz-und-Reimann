54419d5612fc3c24cec64a5252c63707b9a58959
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2022-04-13 12:57:09 +0200

    X is visualized
    - X is shown after button click
    - next test with a button click

--- Visualization - Aufgaben/Assets/Scripts/Scaler.cs
+++ Visualization - Aufgaben/Assets/Scripts/Scaler.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Scaler : MonoBehaviour
 {
-    [SerializeField] float scale = 2;
+    //[SerializeField] float scale = 2;
+    private float scale = 2;
+
+
+    public float Scale => scale;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +18,11 @@ public class Scaler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.localScale != new Vector3 (scale, transform.localScale.y, scale)){
-            transform.localScale = new Vector3 (scale, transform.localScale.y, scale);
-        }
+        
+    }
+
+    public void RandomizeScale(){
+        scale = Random.Range(1.5f, 5);
+        transform.localScale = new Vector3 (scale, transform.localScale.y, scale); 
     }
 }
--- Visualization - Aufgaben/Assets/Scripts/XRechnung.cs
+++ Visualization - Aufgaben/Assets/Scripts/XRechnung.cs
@@ -1,30 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.EventSystems;
+using TMPro;
 
 public class XRechnung : MonoBehaviour
 {   
     public GameObject roterKreis, blauerKreis;
-    private string guessedRatio;
+    public TextMeshProUGUI buttonText; 
+    public GameObject inputField;
+    Scaler blueScaler;
+    private float guessedRatio;
     private float ratio;
+    private float x;
 
-    
+    private void Awake() {
+        blueScaler = blauerKreis.GetComponent<Scaler>();
+        //blueScaler.RandomizeScale();
+
+    }
     // Start is called before the first frame update
     void Start()
-    {
-        GetRadius(roterKreis);
-        GetRadius(blauerKreis);
-        GetSurfaceArea(roterKreis);
-        GetSurfaceArea(blauerKreis);
-        GetRatio(roterKreis, blauerKreis);
+    {   
+        Reset();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Reset() {
+        blueScaler.RandomizeScale();
+        inputField.GetComponent<TMP_InputField>().text = "";
+        buttonText.text = "X berechnen";
+
+        guessedRatio = 0;
+        x = 0;
+        
+        GetRatio(blauerKreis, roterKreis);
     }
 
     float GetRadius(GameObject kreis){
@@ -34,29 +48,37 @@ public class XRechnung : MonoBehaviour
 
     float GetSurfaceArea(GameObject kreis){
         float surfaceArea = Mathf.PI*Mathf.Pow(GetRadius(kreis), 2);
-        Debug.Log("Der " + kreis.name + " hat einen Radius von: " + GetRadius(kreis));
-        Debug.Log("Der Flächeninhalt von " + kreis.name + " beträgt: " + surfaceArea);
         return surfaceArea;
     }
 
-    void GetRatio(GameObject rot, GameObject blau){
+    void GetRatio(GameObject blau, GameObject rot){
         ratio = GetSurfaceArea(blau) / GetSurfaceArea(rot);
+        GetLog(blau);
+        GetLog(rot);
         Debug.Log("Das Verhältnis zwischen dem roten und blauen Kreis ist: 1 zu " + ratio);
     }
 
     public void InputRation(string input){
-        guessedRatio = input;
-        Debug.Log(guessedRatio);
+        guessedRatio = float.Parse(input);
     }
 
     public void GetX(){
-        if(guessedRatio != null){
-            float x = Mathf.Log10(1/float.Parse(guessedRatio))/Mathf.Log10(1/ratio);
-            Debug.Log("X ist: " + x);
+        if(x != 0){
+            Reset();
+        }
+        else if(guessedRatio != 0 && x == 0){
+            x = Mathf.Log10(1/guessedRatio)/Mathf.Log10(1/ratio);
+            inputField.GetComponent<TMP_InputField>().text = "X ist: " + x;
+            //Debug.Log("X ist: " + x);
+            buttonText.text = "Nächster Test";
         }
         else{
             Debug.Log("Es wurde noch nichts eingegeben.");
         }
-    
+    }
+
+    void GetLog(GameObject kreis){
+        Debug.Log("Der " + kreis.name + " hat einen Radius von: " + GetRadius(kreis));
+        Debug.Log("Der Flächeninhalt von " + kreis.name + " beträgt: " + GetSurfaceArea(kreis));
     }
 }
