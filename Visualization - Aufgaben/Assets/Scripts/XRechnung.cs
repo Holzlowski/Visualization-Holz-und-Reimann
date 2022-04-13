6d1aec946a6093774a203d6e01d029ecb7a7aee3
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2022-04-13 10:42:35 +0200

    calculation of x
    added:
    - inputfield for estimated value
    - button for calculation of x

--- Visualization - Aufgaben/Assets/Scripts/XRechnung.cs
+++ Visualization - Aufgaben/Assets/Scripts/XRechnung.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class XRechnung : MonoBehaviour
 {   
     public GameObject roterKreis, blauerKreis;
+    private string guessedRatio;
+    private float ratio;
+
     
     // Start is called before the first frame update
     void Start()
@@ -13,6 +18,7 @@ public class XRechnung : MonoBehaviour
         GetRadius(blauerKreis);
         GetSurfaceArea(roterKreis);
         GetSurfaceArea(blauerKreis);
+        GetRatio(roterKreis, blauerKreis);
     }
 
     // Update is called once per frame
@@ -31,5 +37,26 @@ public class XRechnung : MonoBehaviour
         Debug.Log("Der " + kreis.name + " hat einen Radius von: " + GetRadius(kreis));
         Debug.Log("Der Flächeninhalt von " + kreis.name + " beträgt: " + surfaceArea);
         return surfaceArea;
+    }
+
+    void GetRatio(GameObject rot, GameObject blau){
+        ratio = GetSurfaceArea(blau) / GetSurfaceArea(rot);
+        Debug.Log("Das Verhältnis zwischen dem roten und blauen Kreis ist: 1 zu " + ratio);
+    }
+
+    public void InputRation(string input){
+        guessedRatio = input;
+        Debug.Log(guessedRatio);
+    }
+
+    public void GetX(){
+        if(guessedRatio != null){
+            float x = Mathf.Log10(1/float.Parse(guessedRatio))/Mathf.Log10(1/ratio);
+            Debug.Log("X ist: " + x);
+        }
+        else{
+            Debug.Log("Es wurde noch nichts eingegeben.");
+        }
+    
     }
 }
