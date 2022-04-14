d8973244719766e783b445c7eec865ef96b27e80
dustixx <33931367+dustixx@users.noreply.github.com>
2022-04-14 11:29:04 +0200

    Quader hinzugefügt und bisschen aufgeräumt

--- Visualization - Aufgaben/Assets/Scripts/Scaler.cs
+++ Visualization - Aufgaben/Assets/Scripts/Scaler.cs
@@ -7,22 +7,11 @@ public class Scaler : MonoBehaviour
     //[SerializeField] float scale = 2;
     private float scale = 2;
 
-
     public float Scale => scale;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void RandomizeScale(){
-        scale = Random.Range(1.5f, 5);
+        scale = Random.Range(1.5f, 4.5f);
         transform.localScale = new Vector3 (scale, transform.localScale.y, scale); 
     }
 }
--- Visualization - Aufgaben/Assets/Scripts/XRechnung.cs
+++ Visualization - Aufgaben/Assets/Scripts/XRechnung.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class XRechnung : MonoBehaviour
 {   
     public GameObject roterKreis, blauerKreis;
+    public GameObject roterQuader, blauerQuader;
     public TextMeshProUGUI buttonText; 
     public GameObject inputField;
     Scaler blueScaler;
@@ -13,49 +14,76 @@ public class XRechnung : MonoBehaviour
     private float ratio;
     private float x;
 
-    private void Awake() {
-        blueScaler = blauerKreis.GetComponent<Scaler>();
-        //blueScaler.RandomizeScale();
-
-    }
     // Start is called before the first frame update
     void Start()
     {   
         Reset();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void Reset() {
+        if (blauerKreis.activeInHierarchy)
+            blueScaler = blauerKreis.GetComponent<Scaler>();
+        else if (blauerQuader.activeInHierarchy)
+            blueScaler = blauerQuader.GetComponent<Scaler>();
+
         blueScaler.RandomizeScale();
+
         inputField.GetComponent<TMP_InputField>().text = "";
         buttonText.text = "X berechnen";
 
         guessedRatio = 0;
         x = 0;
-        
-        GetRatio(blauerKreis, roterKreis);
+
+        blauerKreis.transform.position = new Vector3(0, 0, 0);
+        roterKreis.transform.position = new Vector3(-2.5f, 0, 0);
+
+        blauerQuader.transform.position = new Vector3(0, 0, 0);
+        roterQuader.transform.position = new Vector3(-2.5f, 0, 0);
+
+        if (blauerKreis.activeInHierarchy) 
+            GetRatioKreis(blauerKreis, roterKreis);
+        else if (blauerQuader.activeInHierarchy)
+            GetRatioQuader(blauerQuader, roterQuader);
     }
 
     float GetRadius(GameObject kreis){
         float radius = kreis.transform.localScale.z/2;
         return radius;
     }
+    float GetQuaderLength(GameObject quader)
+    {
+        float length = quader.transform.localScale.x;
+        return length;
+    }
 
-    float GetSurfaceArea(GameObject kreis){
+    float GetSurfaceAreaKreis(GameObject kreis){
         float surfaceArea = Mathf.PI*Mathf.Pow(GetRadius(kreis), 2);
         return surfaceArea;
     }
 
-    void GetRatio(GameObject blau, GameObject rot){
-        ratio = GetSurfaceArea(blau) / GetSurfaceArea(rot);
-        GetLog(blau);
-        GetLog(rot);
+    float GetSurfaceAreaQuader(GameObject kreis)
+    {
+        float surfaceArea = kreis.transform.localScale.x * kreis.transform.localScale.z;
+        return surfaceArea;
+    }
+
+    void GetRatioKreis(GameObject blau, GameObject rot){
+        ratio = GetSurfaceAreaKreis(blau) / GetSurfaceAreaKreis(rot);
+        GetLogKreis(blau);
+        GetLogKreis(rot);
         Debug.Log("Das Verhältnis zwischen dem roten und blauen Kreis ist: 1 zu " + ratio);
+        blau.transform.position = new Vector3(blau.transform.localPosition.x + GetRadius(blau)/2, 0, 0);
+        rot.transform.position = new Vector3(rot.transform.localPosition.x - GetRadius(rot), 0, 0);
+    }
+
+    void GetRatioQuader(GameObject blau, GameObject rot)
+    {
+        ratio = GetSurfaceAreaQuader(blau) / GetSurfaceAreaQuader(rot);
+        GetLogQuader(blau);
+        GetLogQuader(rot);
+        Debug.Log("Das Verhältnis zwischen dem roten und blauen Quader ist: 1 zu " + ratio);
+        blau.transform.position = new Vector3(blau.transform.localPosition.x + GetQuaderLength(blau)/2, 0, 0);
+        rot.transform.position = new Vector3(rot.transform.localPosition.x - GetQuaderLength(rot), 0, 0);
     }
 
     public void InputRation(string input){
@@ -77,8 +105,13 @@ public class XRechnung : MonoBehaviour
         }
     }
 
-    void GetLog(GameObject kreis){
+    void GetLogKreis(GameObject kreis){
         Debug.Log("Der " + kreis.name + " hat einen Radius von: " + GetRadius(kreis));
-        Debug.Log("Der Flächeninhalt von " + kreis.name + " beträgt: " + GetSurfaceArea(kreis));
+        Debug.Log("Der Flächeninhalt von " + kreis.name + " beträgt: " + GetSurfaceAreaKreis(kreis));
+    }
+    void GetLogQuader(GameObject quader)
+    {
+        Debug.Log("Der " + quader.name + " hat eine Länge von: " + GetQuaderLength(quader));
+        Debug.Log("Der Flächeninhalt von " + quader.name + " beträgt: " + GetSurfaceAreaQuader(quader));
     }
 }
