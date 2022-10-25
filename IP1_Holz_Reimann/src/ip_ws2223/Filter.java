ccc29781734df455e2b63f93d97e2ca813143364
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2022-10-25 13:15:46 +0200

    Image Processing Aufgabe hinzugefügt

---
+++ IP1_Holz_Reimann/src/ip_ws2223/Filter.java
@@ -0,0 +1,18 @@
+// IP Ue1 WS 2022/23 Vorgabe
+//
+// Copyright (C) 2020 by Klaus Jung
+// All rights reserved.
+// Date: 2020-10-02
+
+package ip_ws2223;
+
+import java.util.Arrays;
+
+public class Filter {
+	
+	public static void outline(RasterImage src, RasterImage dst) {
+		// TODO: implement outline filter
+		Arrays.fill(dst.argb, 0xff00ff00); // green
+	}
+	
+}
